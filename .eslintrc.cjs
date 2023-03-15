8a553ff89827d27068bc88a0905fbaab8b26f2bf
strwatcher <redmoonloon@outlook.com>
2023-03-15 11:05:03 +1000

    setup ts, eslint, prettier, vitest, lefthook on pre-commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    camelcase: 'error',
+    eqeqeq: 'error',
+    'prefer-const': 'error',
+  },
+
+  parser: '@typescript-eslint/parser',
+  root: true,
+
+  ignorePatterns: ['!.*', 'node_modules']
+}
