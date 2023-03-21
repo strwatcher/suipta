94fac758d3f5d575b1500c9f66cbcd6dc10c6f0f
strwatcher <redmoonloon@outlook.com>
2023-03-22 00:36:08 +1000

    feat(tsconfig, package.json, src/index.js): add executable in package.json, now compile tool in js

---
+++ bin/merge/merge.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.merge = void 0;
+function merge(source, additional) {
+    return {
+        ...source,
+        ...additional,
+    };
+}
+exports.merge = merge;
+//# sourceMappingURL=merge.js.map
