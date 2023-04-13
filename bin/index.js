bfee3ae49a78272f098006d2a957a4414fa5bc12
strwatcher <redmoonloon@outlook.com>
2023-04-14 02:36:33 +1000

    build: build

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 
 // src/executable.ts
-import { fork } from "node:child_process";
-import { command, run, string, option, optional } from "cmd-ts";
-import path2 from "node:path";
+import {
+  command,
+  run,
+  string,
+  positional,
+  oneOf
+} from "cmd-ts";
+
+// src/plop/index.ts
+import nodePlop from "node-plop";
 
 // src/helpers/index.ts
 import path, { dirname } from "node:path";
@@ -13,18 +20,69 @@ var __dirname = process.cwd();
 var plopPath = path.join(__packageDir, "plop.js");
 var plopfilePath = path.join(__packageDir, "plopfile.js");
 
+// src/plop/index.ts
+var runPlop = async (args) => {
+  const plop = await nodePlop(plopfilePath);
+  const generator = plop.getGenerator(args.generator);
+  const answers = await generator.runPrompts([args.layer, args.slice]);
+  const result = await generator.runActions(answers);
+  return result;
+};
+
+// src/config/config.default.ts
+var config = {
+  layers: [
+    "shared",
+    "entities",
+    "features",
+    "widgets",
+    "pages",
+    "processes",
+    "app"
+  ],
+  rootDir: "src",
+  templatesDir: "../templates"
+};
+
+// src/config/resolve.ts
+import yaml from "yaml";
+import fs from "node:fs";
+var resolveConfig = (configPath) => {
+  let userConfig;
+  if (configPath.includes("yaml")) {
+    userConfig = loadYamlConfig(configPath);
+  }
+  return { ...config, ...userConfig };
+};
+var loadYamlConfig = (configPath) => {
+  if (!fs.existsSync(configPath)) {
+    return {};
+  }
+  const config3 = yaml.parse(fs.readFileSync(configPath).toString());
+  return config3;
+};
+
+// src/plop/result.ts
+import chalk from "chalk";
+var printResult = (result) => {
+  if (result.failures.length > 0) {
+    result.failures.forEach((failure) => console.log(chalk.red(failure.error)));
+  } else {
+    result.changes.forEach((change) => console.log(chalk.green(change.path)));
+  }
+};
+
 // src/executable.ts
+var config2 = resolveConfig("suipta.config.yaml");
 var app = command({
   name: "suipta",
   args: {
-    configPath: option({
-      type: optional(string),
-      long: "config-path",
-      short: "c"
-    })
+    layer: positional({ type: oneOf(config2.layers), displayName: "layer" }),
+    slice: positional({ type: string, displayName: "slice" })
   },
-  handler: ({ configPath }) => {
-    fork(path2.join(__packageDir, "plop.js"));
+  handler: async ({ layer, slice }) => {
+    const result = await runPlop({ layer, slice, generator: "slice" });
+    printResult(result);
   }
 });
 run(app, process.argv.slice(2));
