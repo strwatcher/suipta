dd1267eadd159ed292f2dc8ba141fb896c355109
strwatcher <redmoonloon@outlook.com>
2023-04-14 02:37:17 +1000

    test: setup testing environment

---
+++ tests/result-check.test.ts
@@ -0,0 +1,47 @@
+import { it, expect, describe, beforeEach } from 'vitest'
+import fs from 'fs'
+import { runPlop } from '../src/plop'
+
+beforeEach(() => {
+  if (fs.existsSync('./tests/src')) {
+    fs.rmSync('./tests/src', { recursive: true, force: true })
+  }
+})
+
+it('on the first creation all should be fine', async () => {
+  const result = await runPlop(
+    {
+      generator: 'slice',
+      layer: 'entities',
+      slice: 'test',
+    },
+    './bin/plopfile.js'
+  )
+  expect(result.failures.length).toBe(0)
+  expect(result.changes.length > 0).toBe(true)
+})
+
+it('if slice already exists failures array should have elements', async () => {
+  const result = await runPlop(
+    {
+      generator: 'slice',
+      layer: 'entities',
+      slice: 'test',
+    },
+    './bin/plopfile.js'
+  )
+  expect(result.failures.length).toBe(0)
+  expect(result.changes.length > 0).toBe(true)
+
+  const secondResult = await runPlop(
+    {
+      generator: 'slice',
+      layer: 'entities',
+      slice: 'test',
+    },
+    './bin/plopfile.js'
+  )
+
+  expect(secondResult.failures.length > 0).toBe(true)
+  expect(secondResult.changes.length).toBe(0)
+})
