63db49b87f1f47f9acc9e173851f1b9e3c690e37
strwatcher <redmoonloon@outlook.com>
2023-05-27 10:26:23 +1000

    feat: add other templates

---
+++ templates/pages/default/model/index.ts
@@ -0,0 +1,7 @@
+export const {{camelCase slice}}Model = () => {
+  
+  return {}
+}
+
+export const $${{camelCase slice}} = {{camelCase slice}}Model()
+
---
+++ templates/widgets/ui/solid/ts/index.tsx
@@ -0,0 +1,6 @@
+import s from './s.module.scss'
+import { {{pascalCase slice }}Props } from '../types'
+
+export const {{ pascalCase slice }} = (props: {{ pascalCase slice }}Props) => {
+  return <>Solid</>
+} 
